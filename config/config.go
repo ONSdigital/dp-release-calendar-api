df5c9d64eaf003d963908227839c1e549c0b7a98
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2023-10-06 13:53:34 +0100

    1834 reorder config

--- config/config.go
+++ config/config.go
@@ -8,11 +8,11 @@ import (
 
 // Config represents service configuration for dp-release-calendar-api
 type Config struct {
+	APIRouterURL               string        `envconfig:"API_ROUTER_URL"`
 	BindAddr                   string        `envconfig:"BIND_ADDR"`
 	GracefulShutdownTimeout    time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
-	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
 	HealthCheckCriticalTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
-	APIRouterURL               string        `envconfig:"API_ROUTER_URL"`
+	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
 }
 
 var cfg *Config
@@ -25,11 +25,11 @@ func Get() (*Config, error) {
 	}
 
 	cfg = &Config{
+		APIRouterURL:               "http://localhost:23200/v1",
 		BindAddr:                   "localhost:27800",
 		GracefulShutdownTimeout:    5 * time.Second,
-		HealthCheckInterval:        30 * time.Second,
 		HealthCheckCriticalTimeout: 90 * time.Second,
-		APIRouterURL:               "http://localhost:23200/v1",
+		HealthCheckInterval:        30 * time.Second,
 	}
 
 	return cfg, envconfig.Process("", cfg)
